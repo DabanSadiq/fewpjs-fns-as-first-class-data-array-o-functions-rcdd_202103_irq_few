f2d4906ff988e96ab0c2e2d69e8c345527ec8078
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 11:48:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+
+function leashDog(dogName, dogBreed) {
+  console.log(`leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed) {
+  console.log(`walk to the park with ${dogName} the ${dogBreed}`);
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName, dogBreed) {
+  console.log(`Walk home with${dogName} the ${dogBreed}`);
+}
+
+function unleashDog(dogName, dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
+
+let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
+
+function exerciseDog(dogName,dogBreed){
+for (var i = 0; i < routine.length; i++) {
+  routine[i](dogName,dogBreed);
+}
+
+  
+}
+
+exerciseDog("zues","g");
