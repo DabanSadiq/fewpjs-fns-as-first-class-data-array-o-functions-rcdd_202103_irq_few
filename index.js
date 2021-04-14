8513aad759e011ce3b890517b83c6ad6eaf622cd
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 11:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
 }
 
 
 function leashDog(dogName, dogBreed) {
-  console.log(`leash ${dogName} the ${dogBreed}`);
+  return `leash ${dogName} the ${dogBreed}`
 }
 
 function walkToPark(dogName, dogBreed) {
-  console.log(`walk to the park with ${dogName} the ${dogBreed}`);
+  return `walk to the park with ${dogName} the ${dogBreed}`
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
 function walkHome(dogName, dogBreed) {
-  console.log(`Walk home with${dogName} the ${dogBreed}`);
+  return `Walk home with${dogName} the ${dogBreed}`
 }
 
 function unleashDog(dogName, dogBreed) {
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`
 }
 
 let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
 
 function exerciseDog(dogName,dogBreed){
+  let str="";
 for (var i = 0; i < routine.length; i++) {
   routine[i](dogName,dogBreed);
+    console.log(str);
 }
 
 
